efc945841f24b8792985327c66505308a31b7b8a
adonai <adonai@in8.com.br>
2022-12-27 18:55:54 -0300

    TEST: controller

--- src/github.controller.spec.ts
+++ src/github.controller.spec.ts
@@ -1,22 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { OCTOKIT, OctokitService } from 'nestjs-octokit';
+import { Octokit } from 'octokit';
+import {
+  mockUserDetail,
+  mockUserList,
+  mockUserRepositories,
+} from '../test/utils/controller.mock';
 import { GitHubController } from './github.controller';
 import { GitHubService } from './github.service';
 
 describe('GitHubController', () => {
   let gitHubController: GitHubController;
-
+  let gitHubService: GitHubService;
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [GitHubController],
-      providers: [GitHubService],
+      providers: [
+        GitHubService,
+        OctokitService,
+        {
+          provide: OCTOKIT,
+          useValue: new Octokit(),
+        },
+      ],
     }).compile();
 
     gitHubController = app.get<GitHubController>(GitHubController);
+    gitHubService = app.get<GitHubService>(GitHubService);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(GitHubController.getHello()).toBe('Hello World!');
+  describe('GitHubController', () => {
+    it('Shold return a users list', async () => {
+      jest
+        .spyOn(gitHubService, 'getUserList')
+        .mockReturnValue(Promise.resolve(mockUserList));
+
+      expect(await gitHubController.usersList('1')).toBe(mockUserList);
+    });
+
+    it('Shold return a user detail', async () => {
+      jest
+        .spyOn(gitHubService, 'getUserDetails')
+        .mockReturnValue(Promise.resolve(mockUserDetail));
+
+      expect(await gitHubController.userDetails('acristoni')).toBe(
+        mockUserDetail,
+      );
+    });
+    it('Shold return a user repositories list', async () => {
+      jest
+        .spyOn(gitHubService, 'getUserRepositories')
+        .mockReturnValue(Promise.resolve(mockUserRepositories));
+
+      expect(await gitHubController.userRepositories('acristoni')).toBe(
+        mockUserRepositories,
+      );
     });
   });
 });
---
+++ test/utils/controller.mock.ts
@@ -0,0 +1,247 @@
+export const mockUserList = {
+  usersList: [
+    {
+      name: 'defunkt',
+      URL: 'https://github.com/defunkt',
+    },
+    {
+      name: 'pjhyett',
+      URL: 'https://github.com/pjhyett',
+    },
+    {
+      name: 'wycats',
+      URL: 'https://github.com/wycats',
+    },
+    {
+      name: 'ezmobius',
+      URL: 'https://github.com/ezmobius',
+    },
+    {
+      name: 'ivey',
+      URL: 'https://github.com/ivey',
+    },
+    {
+      name: 'evanphx',
+      URL: 'https://github.com/evanphx',
+    },
+    {
+      name: 'vanpelt',
+      URL: 'https://github.com/vanpelt',
+    },
+    {
+      name: 'wayneeseguin',
+      URL: 'https://github.com/wayneeseguin',
+    },
+    {
+      name: 'brynary',
+      URL: 'https://github.com/brynary',
+    },
+    {
+      name: 'kevinclark',
+      URL: 'https://github.com/kevinclark',
+    },
+    {
+      name: 'technoweenie',
+      URL: 'https://github.com/technoweenie',
+    },
+    {
+      name: 'macournoyer',
+      URL: 'https://github.com/macournoyer',
+    },
+    {
+      name: 'takeo',
+      URL: 'https://github.com/takeo',
+    },
+    {
+      name: 'caged',
+      URL: 'https://github.com/caged',
+    },
+    {
+      name: 'topfunky',
+      URL: 'https://github.com/topfunky',
+    },
+    {
+      name: 'anotherjesse',
+      URL: 'https://github.com/anotherjesse',
+    },
+    {
+      name: 'roland',
+      URL: 'https://github.com/roland',
+    },
+    {
+      name: 'lukas',
+      URL: 'https://github.com/lukas',
+    },
+    {
+      name: 'fanvsfan',
+      URL: 'https://github.com/fanvsfan',
+    },
+    {
+      name: 'tomtt',
+      URL: 'https://github.com/tomtt',
+    },
+    {
+      name: 'railsjitsu',
+      URL: 'https://github.com/railsjitsu',
+    },
+    {
+      name: 'nitay',
+      URL: 'https://github.com/nitay',
+    },
+    {
+      name: 'kevwil',
+      URL: 'https://github.com/kevwil',
+    },
+    {
+      name: 'KirinDave',
+      URL: 'https://github.com/KirinDave',
+    },
+    {
+      name: 'jamesgolick',
+      URL: 'https://github.com/jamesgolick',
+    },
+    {
+      name: 'atmos',
+      URL: 'https://github.com/atmos',
+    },
+    {
+      name: 'errfree',
+      URL: 'https://github.com/errfree',
+    },
+    {
+      name: 'mojodna',
+      URL: 'https://github.com/mojodna',
+    },
+    {
+      name: 'bmizerany',
+      URL: 'https://github.com/bmizerany',
+    },
+    {
+      name: 'jnewland',
+      URL: 'https://github.com/jnewland',
+    },
+  ],
+  pageSize: 30,
+  page: 1,
+};
+
+export const mockUserDetail = {
+  id: 107071638,
+  name: null,
+  login: 'acristoni',
+  URL: 'https://api.github.com/users/acristoni',
+  crationAccoutDate: '2022-06-07T18:18:14Z',
+};
+
+export const mockUserRepositories = [
+  {
+    id: 518512384,
+    name: 'AirBnb-Clone',
+    URL: 'https://github.com/acristoni/AirBnb-Clone',
+  },
+  {
+    id: 518329655,
+    name: 'api-dioshopping',
+    URL: 'https://github.com/acristoni/api-dioshopping',
+  },
+  {
+    id: 503106504,
+    name: 'app-mosquito',
+    URL: 'https://github.com/acristoni/app-mosquito',
+  },
+  {
+    id: 522710246,
+    name: 'Client_Agencia_Veiculos',
+    URL: 'https://github.com/acristoni/Client_Agencia_Veiculos',
+  },
+  {
+    id: 504265815,
+    name: 'desafio-typescript',
+    URL: 'https://github.com/acristoni/desafio-typescript',
+  },
+  {
+    id: 518501596,
+    name: 'Digital-Business-Card',
+    URL: 'https://github.com/acristoni/Digital-Business-Card',
+  },
+  {
+    id: 518329419,
+    name: 'dioshopping',
+    URL: 'https://github.com/acristoni/dioshopping',
+  },
+  {
+    id: 513355219,
+    name: 'github-api-interface',
+    URL: 'https://github.com/acristoni/github-api-interface',
+  },
+  {
+    id: 523916381,
+    name: 'GraphQL-event-booking',
+    URL: 'https://github.com/acristoni/GraphQL-event-booking',
+  },
+  {
+    id: 501773189,
+    name: 'instagram-dio',
+    URL: 'https://github.com/acristoni/instagram-dio',
+  },
+  {
+    id: 576408541,
+    name: 'JavaScript-Data-Structure',
+    URL: 'https://github.com/acristoni/JavaScript-Data-Structure',
+  },
+  {
+    id: 518514012,
+    name: 'my-travel',
+    URL: 'https://github.com/acristoni/my-travel',
+  },
+  {
+    id: 518515500,
+    name: 'Naruto-Quotes',
+    URL: 'https://github.com/acristoni/Naruto-Quotes',
+  },
+  {
+    id: 569356521,
+    name: 'Portfolio',
+    URL: 'https://github.com/acristoni/Portfolio',
+  },
+  {
+    id: 518494615,
+    name: 'React-Info',
+    URL: 'https://github.com/acristoni/React-Info',
+  },
+  {
+    id: 525875188,
+    name: 'sample-analytics',
+    URL: 'https://github.com/acristoni/sample-analytics',
+  },
+  {
+    id: 525860814,
+    name: 'sample-backend',
+    URL: 'https://github.com/acristoni/sample-backend',
+  },
+  {
+    id: 525875037,
+    name: 'sample-communication',
+    URL: 'https://github.com/acristoni/sample-communication',
+  },
+  {
+    id: 522695486,
+    name: 'Server_Agencia_Veiculos',
+    URL: 'https://github.com/acristoni/Server_Agencia_Veiculos',
+  },
+  {
+    id: 502023339,
+    name: 'Sonic-Pixel-Art',
+    URL: 'https://github.com/acristoni/Sonic-Pixel-Art',
+  },
+  {
+    id: 501003472,
+    name: 'TudoSobreALua',
+    URL: 'https://github.com/acristoni/TudoSobreALua',
+  },
+  {
+    id: 581555486,
+    name: 'youtube-serie-redux-front-end',
+    URL: 'https://github.com/acristoni/youtube-serie-redux-front-end',
+  },
+];
