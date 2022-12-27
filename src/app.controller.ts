08ac6109906c53d886fb867312550b337ee1ebb6
adonai <adonai@in8.com.br>
2022-12-27 08:50:47 -0300

    FEAT: controllers for all routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/api/users')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  usersList(@Query('since') since: number): string {
+    console.log(since);
+    return this.appService.getHello();
+  }
+
+  @Get('/:username/details')
+  userDetails(@Param('username') username: string): string {
+    console.log(username);
+    return this.appService.getHello();
+  }
+
+  @Get('/:username/repos')
+  userRepositories(@Param('username') username: string): string {
+    console.log(username);
     return this.appService.getHello();
   }
 }
