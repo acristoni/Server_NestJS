7f6f3fd366eb4d3f87eab0b60f083e16d00ee296
adonai <adonai@in8.com.br>
2022-12-27 21:37:02 -0300

    TEST: module unit test

---
+++ src/github.module.spec.ts
@@ -0,0 +1,18 @@
+import { Test } from '@nestjs/testing';
+import { GitHubModule } from './github.module';
+import { OctokitModule } from 'nestjs-octokit';
+import { GitHubController } from './github.controller';
+import { GitHubService } from './github.service';
+
+describe('GitHubModule', () => {
+  it('should compile the module', async () => {
+    const module = await Test.createTestingModule({
+      imports: [GitHubModule],
+    }).compile();
+
+    expect(module).toBeDefined();
+    expect(module.get(OctokitModule)).toBeInstanceOf(OctokitModule);
+    expect(module.get(GitHubService)).toBeInstanceOf(GitHubService);
+    expect(module.get(GitHubController)).toBeInstanceOf(GitHubController);
+  });
+});
